6f81ea18d47b64bd5e012391e085fb7079be85f3
Xygardner <xygarde69@gmail.com>
2026-09-11 12:19:14 +0000

    Templates

---
+++ typeConversion/e1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstring>
+
+int main()
+{
+                          // here's int value 3
+    bool f = 3;                         // here's our float variable
+    std::cout << f << '\n';             // print f (containing the bits from n)
+
+    return 0;
+}
---
+++ typeConversion/lmao.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+void foo(int x) {
+std::cout << x;}
+
+void foo(int y) {
+std::cout << y;}
+
+int main (int argc, char *argv[]) {
+  foo(x);
+  foo(y);
+  return 0;
+}
+
+
